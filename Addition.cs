c646db82d8685572673d6795d502886007e08c7d
Swapno <heresnk@outlook.com>
2018-01-14 00:50:09 +0530

    Start Up files and Addition Class file added

---
+++ Addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Addition
+    {
+        public int Add()
+        {
+            Console.WriteLine("Enter the numbers");
+            var x = Convert.ToInt32(Console.ReadLine());
+            var y = Convert.ToInt32(Console.ReadLine());
+            return x + y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Addition add = new Addition();
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1 - For Addition \n 2 - For Substraction \n 3 - For Multiplication \n 4 - For Division");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a == 1)
+            {
+               int c = add.Add();
+               Console.WriteLine("The Sum Value is {0}", c);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
