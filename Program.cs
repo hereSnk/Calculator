9eb2ba85d8dd45a79f2c5f1d2e665cf73d4b274b
Swapno <heresnk@outlook.com>
2018-01-14 19:51:33 +0530

    Substraction

--- Program.cs
+++ Program.cs
@@ -11,14 +11,20 @@ namespace Calculator
         public static void Main(string[] args)
         {
             Addition add = new Addition();
+            substraction sub = new substraction();
             Console.WriteLine("Enter Your Choice");
-            Console.WriteLine("1 - For Addition \n 2 - For Substraction \n 3 - For Multiplication \n 4 - For Division");
+            Console.WriteLine("\n 1 - For Addition \n 2 - For Substraction \n 3 - For Multiplication \n 4 - For Division");
             int a = Convert.ToInt32(Console.ReadLine());
 
             if (a == 1)
             {
                int c = add.Add();
                Console.WriteLine("The Sum Value is {0}", c);
+            }
+            if (a == 2)
+            {
+                int c = sub.Sub();
+                Console.WriteLine("The Difference is {0}", c);
             }
 
             Console.ReadKey();
---
+++ substraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class substraction
+    {
+        public int Sub()
+        {
+            //Console.WriteLine("First Value Should be greater than Second Value");
+            Console.WriteLine("Enter the numbers");
+            var x = Convert.ToInt32(Console.ReadLine());
+            var y = Convert.ToInt32(Console.ReadLine());
+            if(x>y)
+            {
+                return x - y;
+            }
+            else
+            {
+                return y - x;
+            }
+        }
+    }
+}
